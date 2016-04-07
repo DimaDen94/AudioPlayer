be6efddc9a31cac872557be7d5242de966a298df
DimaDen94 <DimaDen94@MAIL.RU>
2016-04-07 19:09:02 +0300

    play after pressing

--- app/src/main/java/com/example/dmitry/audioplayer/PlayListFragment.java
+++ app/src/main/java/com/example/dmitry/audioplayer/PlayListFragment.java
@@ -1,16 +1,31 @@
 package com.example.dmitry.audioplayer;
 
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
-public class PlayListFragment extends Fragment {
+public class PlayListFragment extends Fragment implements View.OnClickListener {
+    private ImageButton buttonPlayStop;
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
+    private final Handler handler = new Handler();
+    int i = 1;
+    private TextView tvTitle;
+    private TextView tvArtist;
+    private TextView tvAlbum;
+
+
 
 
     public void setContext(Context context) {
@@ -31,7 +46,73 @@ public class PlayListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_play_list, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initViews();
+
+    }
+
+
+    private void initViews() {
+        tvTitle = (TextView) getActivity().findViewById(R.id.tvSongTitle);
+        tvArtist = (TextView) getActivity().findViewById(R.id.tvArtist);
+        tvAlbum = (TextView) getActivity().findViewById(R.id.tvAlbum);
+        buttonPlayStop = (ImageButton) getActivity().findViewById(R.id.btn_play_and_pause);
+        buttonPlayStop.setOnClickListener(this);
+        mediaPlayer = MediaPlayer.create(context, R.raw.test_song);
+
+        seekBar = (SeekBar) getActivity().findViewById(R.id.seekBar);
+
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                seekChange(v);
+                return false;
+            }
+        });
+    }
+    private void seekChange(View v){
+        if(mediaPlayer.isPlaying()){
+            SeekBar sb = (SeekBar)v;
+            mediaPlayer.seekTo(sb.getProgress());
+        }
+    }
+    public void playAndStop(){
+        tvTitle.setText("");
+        if (i ==1) {
+            try{
+                mediaPlayer.start();
+                startPlayProgressUpdater();
+            }catch (IllegalStateException e) {
+                mediaPlayer.pause();
+            }
+            i = 0;
+        }else {
+            mediaPlayer.pause();
+            i = 1;
+        }
+    }
+
+    private void startPlayProgressUpdater() {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+
+        if (mediaPlayer.isPlaying()) {
+            Runnable notification = new Runnable() {
+                public void run() {
+                    startPlayProgressUpdater();
+                }
+            };
+            handler.postDelayed(notification,1000);
+        }else{
+            mediaPlayer.pause();
+            i =1;
+            seekBar.setProgress(0);
+        }
     }
 
     @Override
@@ -39,4 +120,9 @@ public class PlayListFragment extends Fragment {
         super.onDetach();
     }
 
+    @Override
+    public void onClick(View v) {
+        seekBar.setMax(mediaPlayer.getDuration());
+        playAndStop();
+    }
 }
